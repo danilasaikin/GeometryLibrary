1271de3953c4b2034f2c2a772f6befbab2b3bd90
Danila Saikin <saykin_777@mail.ru>
2024-05-20 20:36:09 +0300

    Практическое задание maven

---
+++ GeometryUtils/src/main/java/org/example/utils/GeometryUtils.java
@@ -0,0 +1,15 @@
+package org.example.utils;
+
+public class GeometryUtils {
+    // Функция для перевода радиуса круга в диаметр
+    public static double radiusToDiameter(double radius) {
+        return 2 * radius;
+    }
+
+    // Функция для перевода диаметра круга в радиус
+    public static double diameterToRadius(double diameter) {
+        return diameter / 2;
+    }
+
+    // Дополнительные функции могут быть добавлены по мере необходимости
+}
---
+++ src/main/java/org/example/Circle.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    // Новый метод
+    public double calculateDiameter() {
+        return 2 * radius;
+    }
+}
